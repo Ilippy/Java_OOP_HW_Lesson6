d7807947a6bd89cf61cf69c97c4ef20467414d41
Ilippy <Ilippyk@gmail.com>
2023-06-14 21:57:26 +0300

    added third version

--- src/Order.java
+++ src/Order.java
@@ -27,12 +27,12 @@ public class Order {
         this.id = count++;
     }
 
-    public String getClientName() {
-        return client.toString();
+    public Client getClient() {
+        return client;
     }
 
-    public String getProduct() {
-        return product.toString();
+    public Product getProduct() {
+        return product;
     }
 
     public int getQnt() {
@@ -47,54 +47,13 @@ public class Order {
         return id;
     }
 
-    public void saveToJson() {
-        String fileName = "order.json";
-        try (FileWriter writer = new FileWriter(fileName, false)) {
-            writer.write("{\n");
-            writer.write("\"client firstName\":\""+ client.getFirstName() + "\",\n");
-            writer.write("\"client lastName\":\""+ client.getLastName() + "\",\n");
-            writer.write("\"client phoneNumber\":\""+ client.getPhoneNumber() + "\",\n");
-            writer.write("\"product brand\":\""+product.getBrand()+"\",\n");
-            writer.write("\"product name\":\""+product.getName()+"\",\n");
-            writer.write("\"product price\":"+product.getPrice()+",\n");
-            writer.write("\"qnt\":"+qnt+"\n");
-            writer.write("}\n");
-            writer.flush();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
 
     public void inputFromConsole(){
-        String clientFirstName = prompt("Client firstname: ");
-        String clientLastName = prompt("Client lastname: ");
-        String clientPhoneNumber = prompt("Phone number: ");
-
-        if(client != null) {
-            client.setFirstName(clientFirstName);
-            client.setLastName(clientLastName);
-            client.setPhoneNumber(clientPhoneNumber);
-        } else
-            client = new Client(clientFirstName, clientLastName, clientPhoneNumber);
-
-        String productBrand = prompt("Brand name: ");
-        String productName = prompt("Product name: ");
-        double productPrice = Double.parseDouble(prompt("Price: "));
-
-        if(product != null) {
-            product.setBrand(productBrand);
-            product.setName(productName);
-            product.setPrice(productPrice);
-        } else
-            product = new Product(productBrand, productName, productPrice);
-
-        qnt = Integer.parseInt(prompt("Quantity: "));
+        client = Services.getClientFromConsole(client);
+        product = Services.getProductFromConsole(product);
+        qnt = Services.getQuantity();
     }
 
-    private String prompt(String message) {
-        Scanner in = new Scanner(System.in);
-        System.out.print(message);
-        return in.nextLine();
-    }
+
 
 }
